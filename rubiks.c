fb679e503221b55fe9e590ecfb5be9139074b149
Cédric <cedric.yoganathan@efrei.net>
2021-04-23 15:46:13 +0200

    add file

---
+++ rubiks.c
@@ -0,0 +1,4 @@
+//
+// Created by Cedric on 23/04/2021.
+//
+
---
+++ rubiks.h
@@ -0,0 +1,10 @@
+//
+// Created by Cedric on 23/04/2021.
+//
+
+#ifndef PROJET_C_RUBIKS_H
+#define PROJET_C_RUBIKS_H
+
+
+
+#endif //PROJET_C_RUBIKS_H
