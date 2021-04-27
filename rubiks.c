da2b921e58ffffb03754c91856645485821168a4
Cédric <cedric.yoganathan@efrei.net>
2021-04-27 23:49:10 +0200

    side_to_index

--- rubiks.c
+++ rubiks.c
@@ -1,4 +1,28 @@
-//
-// Created by Cedric on 23/04/2021.
-//
+#include <stdio.h>
+#include "rubiks.h"
+
+int select_color(T_COLOR color){
+//TODO
+}
+
+int side_to_index(T_SIDE side){
+    
+    if (side == LEFT){
+        return 0;
+    }else if (side == UP){
+        return 1;
+    }else if (side == FRONT)
+    {
+        return 2;
+    }else if (side == DOWN)
+    {
+        return 3;
+    }else if (side == RIGHT)
+    {
+        return 4;
+    }else if (side == BACK)
+    {
+        return 5;
+    }
+}
 
--- rubiks.h
+++ rubiks.h
@@ -1,7 +1,3 @@
-//
-// Created by Cedric on 23/04/2021.
-//
-
 #ifndef PROJET_C_RUBIKS_H
 #define PROJET_C_RUBIKS_H
 
