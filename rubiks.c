1a2ae1b823f09b60bbb43d2c30cc6525788b8d48
Cédric <cedric.yoganathan@efrei.net>
2021-05-01 11:43:18 +0200

    change side_to_index

--- rubiks.c
+++ rubiks.c
@@ -3,26 +3,27 @@
 
 int select_color(T_COLOR color){
 //TODO
+    return 0;
 }
 
 int side_to_index(T_SIDE side){
-    
-    if (side == LEFT){
-        return 0;
-    }else if (side == UP){
-        return 1;
-    }else if (side == FRONT)
-    {
-        return 2;
-    }else if (side == DOWN)
-    {
-        return 3;
-    }else if (side == RIGHT)
-    {
-        return 4;
-    }else if (side == BACK)
-    {
-        return 5;
+
+    switch (side) {
+
+        case LEFT:
+            return 0;
+        case UP:
+            return 1;
+        case FRONT:
+            return 2;
+        case DOWN:
+            return 3;
+        case RIGHT:
+            return 4;
+        case BACK:
+            return 5;
+        default:
+            return 0;
     }
 }
 
