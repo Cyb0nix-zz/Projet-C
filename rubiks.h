fccd3831e5e4a0454c40804966a62588f8367a62
Cédric <cedric.yoganathan@efrei.net>
2021-05-01 10:46:41 +0200

    defined prototypes

--- rubiks.h
+++ rubiks.h
@@ -3,15 +3,23 @@
 
 typedef enum { FRONT, BACK, UP, DOWN, RIGHT, LEFT } T_SIDE ;
 typedef enum { R, B, G, W, Y, O, LG } T_COLOR ;
-typedef struct {}rubiks;
+typedef struct {
+    T_SIDE Type_face;
+    char Face[3][3];
+    } rubiks_side;
+
+typedef rubiks_side rubiks[6];
 
 int select_color(T_COLOR color);
 int side_to_index(T_SIDE side);
 
-int create_rubiks();
+rubiks* create_rubiks();
 void init_rubiks(rubiks);
 void display_rubiks(rubiks);
-void blank_rubiks();
+void blank_rubiks(rubiks);
+void fill_rubiks(rubiks);
+void scramble_rubiks(rubiks);
+void free_rubiks(rubiks);
 
 
 #endif //PROJET_C_RUBIKS_H
